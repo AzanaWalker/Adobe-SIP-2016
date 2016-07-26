7b13fce7a0f8ff0cf0c8460716859914a0bafc2a
AzanaWalker <azannawalker@gmail.com>
2016-07-26 16:02:10 -0700

    new terrible stuff that didnt work

---
+++ Week5/My Website/scripts.js
@@ -0,0 +1,54 @@
+
+
+//window.onload = function(){
+   // alert("Hi")
+//}
+
+// $('a').click(function(){
+//     $(this).addClass("visited");
+// });
+
+// document.onload= function(){
+//     document.addEventListener('click', changePhoto)
+
+//     function changePhoto(){
+//         console.log("hello")
+//         document.getElementByClassName("about_me")
+        
+//     }
+
+
+// }
+
+
+// $(function(){
+//     // Assume that your 'a' elements are in #MenuBar and #colOne
+//     $(".menu a, #colOne a").bind("click", function(){
+//         // Remove all highlights
+//         $("#MenuBar a, #colOne a").removeClass("clicked");
+
+//         // Store the name attribute
+//         var name = $(this).attr("name");
+
+//         // Find all elements with given name and highlight them
+//         $("#MenuBar a[name='"+name+"'], #colOne a[name='"+name+"']").addClass("clicked");
+//     });
+// });
+
+
+// $(function(){
+//     $(".menu a").bind("click", function(){
+//         $(".menu a").removeClass("clicked"); // Remove all highlights
+//         $(this).addClass("clicked"); // Add the class only for actually clicked element
+//     });
+// });
+
+// function onHover()
+// {
+//     $("#menuImg").attr('src', 'images/IMG_9262.jpg');
+// }
+
+// function offHover()
+// {
+//     $("#menuImg").attr('src', 'images/IMG_3015.jpg');
+// }
